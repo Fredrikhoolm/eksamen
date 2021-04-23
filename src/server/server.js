b269c7396c37c52217a16c94535875e69c9f808e
Fredrik Holm Eikrem <fredrik_eikrem@hotmail.com>
2021-04-23 17:53:34 +0200

    Created a start to exam

---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const wsServer = require("./websocket");
+app.use("/api", require("./apiRouter"));
+
+app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(
+      path.resolve(__dirname, "..", "..", "dist", "index.html")
+    );
+  }
+  return next();
+});
+
+const server = app.listen(3000, () => {
+  server.on("upgrade", (req, socket, head) => {
+    wsServer.handleUpgrade(req, socket, head, (socket) => {
+
+      wsServer.emit("connection", socket, req);
+    });
+  });
+});
